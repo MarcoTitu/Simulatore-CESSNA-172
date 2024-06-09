fd8e17af49e88b0840cc2d9f676f1e197cb8ccdb
MarcoTitu <163676038+MarcoTitu@users.noreply.github.com>
2024-06-09 16:09:51 +0200

    Add files via upload

---
+++ Main/Librerie/trim.h
@@ -0,0 +1,257 @@
+#ifndef TRIM_H
+#define TRIM_H
+
+#include "interpola.h"
+#include "propel.h"
+
+extern double engine[7];
+extern double general_data[30];
+extern double alpha[126];
+extern double Cx[126], Cy[126], Cz[126], Cl[126], Cm[126], Cn[126];
+extern double Cxa[126], Cxap[126], Cxu[126], Cxq[126], Cxb[126], Cxp[126], Cxr[126];
+extern double Cyb[126], Cybp[126], Cyp[126], Cyr[126], Cya[126], Cyq[126];
+extern double Czalpha[126], Czap[126], Czu[126], Czq[126], Czb[126], Czp[126], Czr[126];
+extern double ClB[126], ClBP[126], ClP[126], ClR[126], ClA[126], ClQ[126];
+extern double CmA[126], CmAP[126], CmU[126], CmQ[126], CmB[126], CmP[126], CmR[126];
+extern double CnB[126], CnBP[126], CnP[126], CnR[126], CnA[126], CnQ[126];
+extern double CXde[126], CXdle[126], CZde[126], CZdle[126], CYda[126], CYdr[126];
+extern double Clda[126], Cldr[126], Cmde[126], Cmdle[126], Cnda[126], Cndr[126];
+extern double CXom[126], CYom[126], CZom[126], Clom[126], Cmom[126], Cnom[126];
+double press0, temp0, rho0, vsuono0;                    // Valori atmosfera a quota 0
+double press_h, temp_h, rho_h, vsuono_h, P_max_h, mach; // Valori atmosfera a quota h scelta
+double trim_angles[2], RPM_trim, P_max_h;
+extern short int flag_eq_equilibratore;
+extern double propThrust;
+extern double propTorque;
+extern double P_trim;
+
+double delta_e_calc(double alpha_int)
+{
+
+    // leggere cm vari e verificare equazione di momento
+
+    double delta_e_min = general_data[23];
+    double delta_e_max = general_data[22];
+    double delta_e = delta_e_min;
+    double delta_e_trim = 0;
+    double cm_ss = interpola(alpha, Cm, alpha_int);
+    double cm_alpha = interpola(alpha, CmA, alpha_int);
+    double cm_de = interpola(alpha, Cmde, alpha_int);
+    double delta_delta_e = 0.01; // diminuire se non converge o se da richiesta
+    double delta_e_rad;
+    double alpha_int_rad = alpha_int * M_PI / 180.0;
+
+    double de_analitico = (-cm_ss - cm_alpha * alpha_int_rad) / interpola(alpha, Cmde, alpha_int);
+    delta_e = de_analitico;
+    delta_e_trim = delta_e * 180 / M_PI;
+
+    // printf("Delta_e (analitico): %lf", de_analitico*180/M_PI);
+    // FINE TEST
+
+    // while (delta_e <= delta_e_max){
+    //     delta_e_rad = delta_e*M_PI/180.0;
+    //     double condition = cm_ss + cm_alpha*alpha_int_rad + cm_de*delta_e_rad;
+    //     double res = 0.0005;          // scegliere in modo piu' furbo la tolleranza
+
+    //     if (fabs(condition) < res){
+    //         delta_e_trim = delta_e;
+    //         break;
+    //     }
+
+    //     else{
+    //         delta_e = delta_e + delta_delta_e;
+    //     }
+    // }
+
+    if (delta_e_trim > delta_e_max || delta_e_trim < delta_e_min)
+    {
+        flag_eq_equilibratore = -1;
+    }
+    else
+    {
+        flag_eq_equilibratore = 0;
+    }
+
+    return delta_e_trim;
+}
+
+int alpha_trim_calc(double V, double h, double rho, double gamma, double g, double trim_angles[])
+{
+
+    /*
+    input utente sono V, h, gamma (gamma sara' nullo in primo tentativo)
+    si parlava di avere V, h, rho come variabili globali (quindi non c'e' bisogno di passarle)
+    */
+
+    double delta_e_min = general_data[23];
+    double delta_e_max = general_data[22];
+    double m = general_data[0];
+    double S = general_data[2];
+    double alpha_int = alpha[0];
+    double alpha_max = 20;
+    double delta_alpha = 0.001;
+    double alpha_trim = alpha_int;
+    double Cz_delta_e = interpola(alpha, CZde, alpha_int);
+    double Cz_alpha = interpola(alpha, Czalpha, alpha_int);
+    double delta_e_trim = 0;
+    short int flag_alfa = 0;
+
+    printf("\nCalcolo alpha e delta equilibratore...\n");
+
+    while (alpha_int <= alpha_max)
+    {
+        Cz_delta_e = interpola(alpha, CZde, alpha_int);
+        Cz_alpha = interpola(alpha, Czalpha, alpha_int);
+        // BISOGNA USARE GLI ANGOLI IN RADIANTI
+        double alpha_int_rad = alpha_int * M_PI / 180.0;
+        // Se alpha viene passato in gradi ricordiamoci la conversione in delta_e_calc
+        delta_e_trim = delta_e_calc(alpha_int);
+
+        double Cz_ss_interp = interpola(alpha, Cz, alpha_int);
+        double Cz_tot = Cz_ss_interp + Cz_alpha * alpha_int_rad + Cz_delta_e * delta_e_trim * M_PI / 180.0;
+        // gamma è in gradi e va convertito
+        double theta = alpha_int_rad + gamma * M_PI / 180.0;
+        double condition = m * g * cos(theta) + 0.5 * Cz_tot * rho * S * (V * V);
+        // DEBUG
+        // printf("Alpha corrente: %lf \t Theta (rad): %lf \t  Delta_e: %lf\t Risultante su Z: %lf\n", alpha_int, theta, delta_e_trim, condition);
+
+        double res = 10; // [N]
+
+        if (fabs(condition) < res)
+        {
+            alpha_trim = alpha_int;
+            if (flag_eq_equilibratore != 0)
+            {
+                printf("WARNING: Non trovato valore di delta_e di trim\n");
+                if (delta_e_trim < 0)
+                {
+                    delta_e_trim = delta_e_min;
+                    printf("delta_e limitato a -20 deg\n");
+                }
+                if (delta_e_trim > 0)
+                {
+                    delta_e_trim = delta_e_max;
+                    printf("delta_e limitato a 20 deg\n");
+                }
+            }
+            // Rimuovere se si torna a metodo non analitico
+            // printf("ATTENZIONE: angolo di equilibratore ottenuto analiticamente!\n");
+            // printf("Alpha trim: %lf \t Delta eq. trim: %lf\n", alpha_int, delta_e_trim);
+            flag_alfa = 0;
+            break;
+        }
+        else
+        {
+            alpha_int = alpha_int + delta_alpha;
+        }
+    }
+
+    if (alpha_int > alpha_max)
+    {
+        printf("ERRORE -5: Non trovato valore di alfa di trim\n");
+        printf("Premere invio per inserire una nuova condizione di trim...\n");
+        getchar();
+        getchar();
+        system("cls");
+        flag_alfa = -1;
+    }
+    trim_angles[0] = alpha_trim;
+    trim_angles[1] = delta_e_trim;
+
+    printf("Alpha Trim: \t %lf [deg]\n", trim_angles[0]);
+    printf("delta Trim: \t %lf [deg]\n", trim_angles[1]);
+
+    return flag_alfa;
+}
+
+double rpm_trim_calc(double alpha_trim, double delta_e_trim, double V, double h, double rho, double gamma, double g, double RPM_min, double RPM_max)
+{
+
+    double omega_trim;
+    int flag_RPM;
+    double m = general_data[0];
+    double S = general_data[2];
+    double RPM = RPM_min;
+    double delta_RPM = 1; // [rpm] come prima approx, poi a discrezione utente
+    double Cx_delta_e = interpola(alpha, CXde, alpha_trim);
+    double Cx_alpha = interpola(alpha, Cxa, alpha_trim);
+    double theta_trim = alpha_trim + gamma;
+    theta_trim = theta_trim * M_PI / 180.0;
+    P_max_h = engine[0] * pow(rho_h / (rho0), engine[1]);
+
+    printf("\nCalcolo Parametri Motore...\n");
+
+    while (RPM <= RPM_max)
+    {
+        propel(V, RPM, rho); // da propel.c
+        double T = propThrust;
+        double Cx_tot = interpola(alpha, Cx, alpha_trim) + Cx_alpha * alpha_trim * M_PI / 180.0 + Cx_delta_e * delta_e_trim * M_PI / 180.0;
+        double T_trim = m * g * sin(theta_trim) - 0.5 * Cx_tot * rho * S * (V * V);
+        double condition = T_trim - T;
+        double res = 2;
+
+        if (fabs(condition) <= res)
+        {
+            RPM_trim = RPM;
+            omega_trim = 2 * M_PI / 60 * RPM_trim;
+            P_trim = propTorque * omega_trim / 1000; // [kW]
+            if (P_trim > P_max_h)
+            {
+                printf("WARNING: Potenza massima superata\nPotenza limitata alla potenza massima\n"); // Messaggio di warning
+                P_trim = P_max_h;
+            }
+            break;
+        }
+        else
+        {
+            RPM = RPM + delta_RPM;
+        }
+    }
+
+    if (RPM > RPM_max)
+    {
+        printf("WARNING: Convergenza non raggiunta su RPM; \nRPM impostati al valore medio\n");
+        RPM_trim = (RPM_max + RPM_min) / 2;
+        propel(V, RPM_trim, rho); // da propel.c
+        omega_trim = 2 * M_PI / 60 * RPM_trim;
+        P_trim = propTorque * omega_trim / 1000; // [kW]
+    }
+    printf("Thrust: \t %lf [N]\n", propThrust);
+    printf("Torque: \t %lf [Nm]\n", propTorque);
+    printf("RPM: \t\t %lf\n", RPM_trim);
+    printf("Power: \t\t %lf [kW]\n", P_trim);
+
+    return flag_RPM;
+}
+
+void vett_stat_trim(double trim_angles[], double vett_stato[], double CI[])
+{
+    double alpha_trim = trim_angles[0];
+    double delta_e_trim = trim_angles[1];
+    double V = CI[0];
+    double h = CI[1];
+    double gamma = CI[2];
+
+    vett_stato[0] = V * cos(alpha_trim * M_PI / 180);
+    vett_stato[1] = 0;
+    vett_stato[2] = V * sin(alpha_trim * M_PI / 180);
+    vett_stato[3] = 0;
+    vett_stato[4] = 0;
+    vett_stato[5] = 0;
+    vett_stato[6] = 0;
+    vett_stato[7] = alpha_trim + gamma;
+    vett_stato[8] = 0;
+    vett_stato[9] = h;
+    vett_stato[10] = 0;
+    vett_stato[11] = 0;
+    vett_stato[12] = general_data[0];
+    vett_stato[13] = propThrust;
+    vett_stato[14] = P_trim * 1000;
+}
+
+void vett_com_trim(double trim_angles[], double vett_comm[], double RPM_trim)
+{
+    vett_comm[0] = trim_angles[1];
+    vett_comm[3] = (RPM_trim - engine[2]) / (engine[3] - engine[2]) * 100;
+}
+#endif
---
+++ Main/Main.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Librerie
+#include "..\Main\Librerie\Atmosphere.h"
+#include "..\Main\Librerie\interpola.h"
+#include "..\Main\Librerie\propel.h"
+#include "..\Main\Librerie\read.h"
+#include "..\Main\Librerie\stabilita.h"
+#include "..\Main\Librerie\trim.h"
+#include "..\Main\Librerie\integrazione.h"
+#include "..\Main\Librerie\comandi.h"
+
+// Variabili
+
+double engine[7];                                           // Variabili lette dal file engine.txt
+double propeller_data[11], CSI[60], RD[60], CH[60], BA[60]; // Variabili lette dal file propeller.txt
+double general_data[30], alpha[126];                        // dati generali letti dal file dba.txt
+
+// Derivate aerodinamiche lette dal file dba.txt
+double Cx[126], Cy[126], Cz[126], Cl[126], Cm[126], Cn[126];
+double Cxa[126], Cxap[126], Cxu[126], Cxq[126], Cxb[126], Cxp[126], Cxr[126];
+double Cyb[126], Cybp[126], Cyp[126], Cyr[126], Cya[126], Cyq[126];
+double Czalpha[126], Czap[126], Czu[126], Czq[126], Czb[126], Czp[126], Czr[126];
+double ClB[126], ClBP[126], ClP[126], ClR[126], ClA[126], ClQ[126];
+double CmA[126], CmAP[126], CmU[126], CmQ[126], CmB[126], CmP[126], CmR[126];
+double CnB[126], CnBP[126], CnP[126], CnR[126], CnA[126], CnQ[126];
+double CXde[126], CXdle[126], CZde[126], CZdle[126], CYda[126], CYdr[126];
+double Clda[126], Cldr[126], Cmde[126], Cmdle[126], Cnda[126], Cndr[126];
+double CXom[126], CYom[126], CZom[126], Clom[126], Cmom[126], Cnom[126];
+
+double press0 = 101325, temp0 = 15, rho0 = 1.225, vsuono0 = 340;       // Valori atmosfera a quota 0
+double press_h, temp_h, rho_h, vsuono_h, P_max_h, mach;                // Valori atmosfera a quota h scelta
+double propThrust, propTorque, P_trim;                                 // Spinta e Coppia motore
+double CI[3] = {0, 0, 0};                                              // Condizioni iniziali di velocità, quota e rampa {V,h,gamma}
+double trim_angles[2], RPM_trim, g = 9.81;                             // angoli di trim {alfa, delta_e} e RPM di trim
+double vett_stato[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Vettore di stato istante iniziale {u,v,w,p,q,r,phi,theta,psi,h,x,y,massa,T,P}
+double vett_comm[4] = {0, 0, 0, 0};                                    // Vettore dei comandi istante iniziale {delta_e,delta_a,delta_r,RPM}
+short int flag_eq_equilibratore = 0;
+int flagatm;
+double fugoide[4], CP[4]; // Valori di fugoide e corto periodo
+
+int file_count;
+char file_count_char[20];
+
+double t_trim = 1000, t_sim;
+
+int main()
+{
+    ////// INTESTAZIONE /////
+    // printf("\t\t\t       _____\n");
+    // printf("\t\t\t         |\n");
+    // printf("\t\t\t\\______0(.)0______/\n\n");
+    printf("\t\t\t\t\t|\n");
+    printf("\t  ______________________________|_______________________________\n");
+    printf("\t\t\t      ----\\--||___||--/----\n");
+    printf("\t\t\t\t   \\ :==e==: /\n");
+    printf("\t\t\t\t    \\|  o  |/\n");
+    printf("\t\t\t\t     \\_____/\n");
+    printf("\t\t\t\t     /  |  \\ \n");
+    printf("\t\t\t\t   e/   e   \\e\n");
+    printf("\t\t\t\t   U    U    U\n");
+    printf("\t\t\t        SIMULATORE DI VOLO\n");
+    printf("\t\t\t       Politecnico di Torino\n");
+    printf("\t\t    Corso di Simulazione del Volo - Luglio 2024\n");
+    printf("\t\t  Sviluppato da: GRUPPO 03 per Cessna 172 Skyhawk\n");
+
+    printf("\033[?25l"); // nasconde il cursore
+    printf("\nPremere invio per avviare il simulatore\n");
+    getchar();
+    system("cls");
+
+    ///// LETTURA FILE ////
+
+    printf("Caricamento file database aerodinamico\n");
+    readDba();
+    printf("Database aerodinamico letto correttamente...\n\n");
+    printf("Caricamento file database motore\n");
+    readEngine();
+    printf("Database motore letto correttamente...\n\n");
+    printf("Caricamento file database propeller\n");
+    readPropeller();
+    printf("Database propeller letto correttamente...\n\n");
+    printf("Premere invio per continuare");
+    getchar();
+    system("cls");
+
+    ////// ANALISI TRIM /////
+
+    // INPUT CONDIZIONI INIZIALI //
+
+    printf("\033[?25h"); // Mostra il cursore
+    char input_utente;
+    int flag_alfa, flag_RPM, flag_stabilita;
+    do
+    {
+        do
+        {
+            input_utente = '\0';
+            do
+            {
+                system("cls");
+                flagatm = AtmosphereChoice();
+            } while (flagatm != 0);
+
+            printf("Conferma Input? [y/n] ");
+            scanf(" %c", &input_utente);
+            if (input_utente != 'y' && input_utente != 'n')
+            {
+                printf("Inserire y o n! ");
+                scanf(" %c", &input_utente);
+            }
+            system("cls");
+        } while (input_utente != 'y');
+
+        // CONDIZIONI DI TRIM //
+
+        printf("\nInizio analisi condizioni trim...\n");
+
+        flag_alfa = alpha_trim_calc(CI[0], CI[1], rho_h, CI[2], g, trim_angles);
+        flag_RPM = rpm_trim_calc(trim_angles[0], trim_angles[1], CI[0], CI[1], rho_h, CI[2], g, engine[2], engine[3]);
+        if (flag_alfa == 0)
+        {
+            flag_stabilita = stabilita(trim_angles[1], CI, rho_h, trim_angles[0], g);
+            vett_stat_trim(trim_angles, vett_stato, CI);     // Scrivi su vett di stato
+            vett_com_trim(trim_angles, vett_comm, RPM_trim); // Scrivi su vett comandi
+        }
+
+    } while (flag_alfa != 0 && flag_stabilita != 0);
+
+    // RICHIESTA PER PROCEDERE CON LA MANOVRA //
+
+    input_utente = '\0';
+    printf("\n\nSi vuole procedere con l'analisi delle manovre? [y/n] ");
+    scanf(" %c", &input_utente);
+    if (input_utente != 'y' && input_utente != 'n')
+    {
+        printf("inserire y o n! ");
+        scanf(" %c", &input_utente);
+    }
+    if (input_utente == 'n')
+    {
+        printf("\033[?25l"); // nasconde il cursore
+        printf("\n\nSIMULATORE ARRESTATO. \nPremere invio per chiudere");
+        getchar();
+        getchar();
+        return 0;
+    }
+
+    system("cls");
+    printf("\n\t\tANALISI DI MANOVRA\n\n");
+    printf("condizione di trim iniziale: | V = %.2lf m/s | h = %.2lf m | Angolo di rampa %.2lf deg | \n", CI[0], CI[1], CI[2]);
+
+    // MANTENIMENTO CONDIZIONI DI TRIM //
+
+    printf("\nSi vuole effettuare l'analisi di mantenimento del trim? [y/n] ");
+    scanf(" %c", &input_utente);
+    if (input_utente != 'y' && input_utente != 'n')
+    {
+        printf("Inserire y o n! ");
+        scanf(" %c", &input_utente);
+    }
+
+    if (input_utente == 'y')
+    {
+        system("cls");
+        input_utente = '\0';
+        file_count = 0;
+        comandi_trim(vett_comm);
+        // Integrazione delle equazioni del moto
+        printf("MANOVRA - Mantenimento del trim\nSimulazione in corso: \n");
+        integrazione(0.01, t_trim);
+        printf("\033[?25l"); // nasconde il cursore
+        printf("Premere invio per continuare");
+        getchar();
+        getchar();
+    }
+    file_count = 1;
+    do
+    {
+        printf("\033[?25h"); // Mostra il cursore
+        input_utente = '\0';
+        system("cls");
+        printf("condizione di trim iniziale: | V = %.2lf m/s | h = %.2lf m | Angolo di rampa %.2lf deg | \n", CI[0], CI[1], CI[2]);
+        printf("\nSi vuole procedere all'analisi di una nuova manovra con queste condizioni iniziali? [y/n] ");
+        scanf(" %c", &input_utente);
+        if (input_utente != 'y' && input_utente != 'n')
+        {
+            printf("Inserire y o n! ");
+            scanf(" %c", &input_utente);
+        }
+        if (input_utente == 'n')
+        {
+            break;
+        }
+
+        printf("\nInserire la durata della manovra: ");
+        int f = scanf("%lf", &t_sim);
+        getchar();
+        // if(t_sim>600){
+        //     printf("Si e' inserito un tempo superiore ai 600 secondi\n La simulazione si arrestera' a 600 secondi\n");
+        //     t_sim=600;
+        // }
+        genera_comandi(t_sim / 0.01, 0.01, vett_comm);
+        printf("Premere invio per continuare");
+        getchar();
+        system("cls");
+        // Integrazione delle equazioni del moto
+        printf("MANOVRA \nSimulazione in corso: \n");
+        integrazione(0.01, t_sim);
+        printf("\033[?25l"); // nasconde il cursore
+        printf("Premere invio per continuare");
+        getchar();
+
+        file_count++;
+
+    } while (input_utente != 'n');
+
+    printf("\033[?25l"); // nasconde il cursore
+    printf("\nSIMULAZIONE TERMINATA\n");
+    printf("Premere invio per chiudere");
+    getchar();
+    getchar();
+
+    return 0;
+}
