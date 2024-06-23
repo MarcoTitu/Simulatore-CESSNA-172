d342bb72a1f4ffebb794959a7126575f937a291f
MarcoTitu <marcox2302@gmail.com>
2024-06-23 18:03:15 +0200

    salvataggio immagini post-processing

--- Main/postProc.m
+++ Main/postProc.m
@@ -16,6 +16,15 @@ dataTab_sv = readtable(FilePath_sv,'DecimalSeparator', '.','NumHeaderLines', 0);
 data_sv=dataTab_sv{:,:};
 headers_sv=["t", "u [m/s]", "v [m/s]", "w [m/s]", "p [rad/s]", "q [rad/s]", "r [rad/s]", "\phi [deg]", "\theta [deg]", "\psi [deg]", "h [m]", "x [m]", "y [m]", "m [kg]"];
 
+%% destinazione figure
+
+% Ottieni il percorso completo della cartella in cui si trova il file .m
+cartella_corrente = pwd;
+
+% Crea la sottocartella 'figure' nella cartella corrente
+sottocartella = 'Output_post-processing';
+cartella_destinazione = fullfile(cartella_corrente, sottocartella);
+mkdir(cartella_destinazione);
 
 %% Plot commands
 data_c(:,5)=100*data_c(:,5); %Adjust throttle percentage
@@ -30,6 +39,9 @@ for i=2:length(data_c(1,:))
     sgtitle('Andamenti nel tempo di \delta_e, \delta_a, \delta_r, \pi', 'fontname', 'Times New Roman', 'FontSize', 16);
 end
 
+saveas(gcf, fullfile(cartella_destinazione, 'figure_comandi.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_comandi.fig'));
+close all
 
 %% Plot all state variables
 figure(2)
@@ -41,6 +53,9 @@ for i=2:4
     xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14);
     sgtitle ('Andamenti nel tempo di u, v, w', 'fontname', 'Times New Roman', 'FontSize', 16) 
 end
+saveas(gcf, fullfile(cartella_destinazione, 'figure_uvw.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_uvw.fig'));
+close all
 
 figure(3)
 for i=5:7
@@ -51,6 +66,9 @@ for i=5:7
     xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14);
     sgtitle ('Andamenti nel tempo di p, q, r', 'fontname', 'Times New Roman', 'FontSize', 16) 
 end
+saveas(gcf, fullfile(cartella_destinazione, 'figure_pqr.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_pqr.fig'));
+close all
 
 figure(4)
 for i=8:10
@@ -61,6 +79,9 @@ for i=8:10
     xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14);
     sgtitle ('Andamenti nel tempo di \phi, \theta, \psi', 'fontname', 'Times New Roman', 'FontSize', 16) 
 end
+saveas(gcf, fullfile(cartella_destinazione, 'figure_angles.fig'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_angles.png'));
+close all
 
 figure(5)
 plot(data_sv(:,1), data_sv(:,11),'b',LineWidth=2)
@@ -78,6 +99,9 @@ for i=11:13
     xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14)
     sgtitle ('Andamenti nel tempo di quota, x, y', 'fontname', 'Times New Roman', 'FontSize', 16) 
 end
+saveas(gcf, fullfile(cartella_destinazione, 'figure_NED.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_NED.fig'));
+close all
 
 figure(7)
 plot(data_sv(:,1), data_sv(:,14),'b',LineWidth=2)
@@ -85,6 +109,10 @@ grid on
 ylabel(headers_sv(14), 'fontname', 'Times New Roman', 'FontSize', 14)
 xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14)
 sgtitle ('Andamento nel tempo della massa', 'fontname', 'Times New Roman', 'FontSize', 16) 
+saveas(gcf, fullfile(cartella_destinazione, 'massa.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'massa.fig'));
+close all
+
 
 %% Plot velocità V
 figure(8)
@@ -104,6 +132,9 @@ ylabel('Velocità V[m/s]' ,'fontname','Times New Roman','FontSize',14,'color',[0
 title('Andamento della velocità nel tempo' ,'fontname','Times New Roman','FontSize',16)
 legend ('V di stallo','V_{NE}', 'V')
 axis([data_sv(1,1) data_sv(end,1) 25 80])
+saveas(gcf, fullfile(cartella_destinazione, 'figure_velocità.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_velocità.fig'));
+close all
 
 
 %% Plot alpha e beta
@@ -121,6 +152,9 @@ grid on
 xlabel("Time [s]")
 ylabel("\beta [deg]")
 title("Andamento nel tempo di \beta", 'fontname', 'Times New Roman', 'FontSize', 16)
+saveas(gcf, fullfile(cartella_destinazione, 'figure_alfabeta.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_alfabeta.fig'));
+close all
 
 
 %% Plot traiettoria 3D
@@ -139,6 +173,9 @@ legend('Traiettoria', 'Partenza', 'Arrivo')
 % zlim([min(data_sv(:,11))-1000 max(data_sv(:,11))+1000])
 zlim([0 4116])
 set(gca, 'YDir','reverse')
+saveas(gcf, fullfile(cartella_destinazione, 'figure_traiettoria3D.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_traiettoria3D.fig'));
+close all
 
 
 %% Plot traiettoria 2D
@@ -155,6 +192,9 @@ legend('Traiettoria', 'Partenza', 'Arrivo');
 set(gca, 'YDir','reverse')
 hold off
 grid on
+saveas(gcf, fullfile(cartella_destinazione, 'figure_traiettoria2D.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_traiettoria2D.fig'));
+close all
 
 
 %% Andamento spinta e quota nel tempo
@@ -165,6 +205,9 @@ ylabel('Thrust [N]', 'fontname', 'Times New Roman', 'FontSize', 14)
 xlabel("Time[s]", 'fontname', 'Times New Roman', 'FontSize', 14)
 sgtitle ('Andamento nel tempo della spinta', 'fontname', 'Times New Roman', 'FontSize', 16) 
 
+saveas(gcf, fullfile(cartella_destinazione, 'figure_spinta.png'));
+saveas(gcf, fullfile(cartella_destinazione, 'figure_spinta.fig'));
+close all
 
 
 
